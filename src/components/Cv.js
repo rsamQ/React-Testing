6420848209a6ea4c73aff69029b64f9debc8e979
rsam <sammeth-rene@web.de>
2021-06-05 21:54:19 +0200

    added typewriter and fixes to navbar

--- src/components/Cv.js
+++ src/components/Cv.js
@@ -4,18 +4,22 @@ import { CvItems } from './CvItems';
 export default class Cv extends Component {
     render(){
         return (
-            <div id="timeline-content">
-                <h1>Lebenslauf</h1>
-                <ul className="timeline">
-                    {CvItems.map((item, index) =>{
-                        return(
-                            <li key={index} className={item.style} data-date={item.date}>
-                            <h3>{item.title}</h3>
-                            <p>{item.description}</p>
-                        </li>
-                        )
-                    })}
-                </ul>
+            <div>
+                <div className="typewriter">
+                    <h1>Lebenslauf</h1>
+                </div>
+                <div id="timeline-content">
+                    <ul className="timeline">
+                        {CvItems.map((item, index) =>{
+                            return(
+                                <li key={index} className={item.style} data-date={item.date}>
+                                <h3>{item.title}</h3>
+                                <p>{item.description}</p>
+                            </li>
+                            )
+                        })}
+                    </ul>
+                </div>
             </div>
         )
     }
